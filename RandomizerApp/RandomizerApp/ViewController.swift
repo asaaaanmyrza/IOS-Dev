b76a462820f026344edd3ab8228e3fc1e3aa0757
Asan Myrzakhmetov <156882836+asaaaanmyrza@users.noreply.github.com>
2025-10-18 18:43:17 +0500

    Add files via upload

--- RandomizerApp/RandomizerApp/ViewController.swift
+++ RandomizerApp/RandomizerApp/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var ItemImage: UIImageView!
     
+    @IBOutlet weak var ItemDescription: UILabel!
+    
     @IBOutlet weak var Button: UIButton!
     
     @IBOutlet weak var ItemTitle: UILabel!
@@ -18,6 +20,7 @@ class ViewController: UIViewController {
     
     let randomItems: [String] = ["billie joe", "Wildberries bag", "sticky notes", "eki myn tenge", "jent", "periodic table", "screw", "sponges", "lace", "seat belt"]
     let randomImages: [UIImage] = [#imageLiteral(resourceName: "billie_joe"), #imageLiteral(resourceName: "wildberries"), #imageLiteral(resourceName: "sticky_note"), #imageLiteral(resourceName: "money"), #imageLiteral(resourceName: "jent"), #imageLiteral(resourceName: "periodic_table"), #imageLiteral(resourceName: "screw"), #imageLiteral(resourceName: "sponge"), #imageLiteral(resourceName: "lace"), #imageLiteral(resourceName: "seat_belt")]
+    let randomDescriptions: [String] = ["great singer", "cute bag", "helpful when studying", "not enough", "damdi", "chemistry thing", "its a screw!", "for cleaning", "cute lace", "for safety"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +32,7 @@ class ViewController: UIViewController {
         let randomIndex = Int.random(in: 0...9)
         ItemTitle.text = String(randomItems[randomIndex])
         ItemImage.image = randomImages[randomIndex]
+        ItemDescription.text = randomDescriptions[randomIndex]
     }
     
     
